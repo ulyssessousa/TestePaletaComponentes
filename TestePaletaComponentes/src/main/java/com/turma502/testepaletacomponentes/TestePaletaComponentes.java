fd123e957aa1174028cb867ef8659be8a24c7bba
ulyssessousa <ulyssessousa@gmail.com>
2025-05-23 11:12:50 -0300

    first commit

---
+++ TestePaletaComponentes/src/main/java/com/turma502/testepaletacomponentes/TestePaletaComponentes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.turma502.testepaletacomponentes;
+
+/**
+ *
+ * @author ulyss
+ */
+public class TestePaletaComponentes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
